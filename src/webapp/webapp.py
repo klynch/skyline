ff2824e1795ae436e98be6277a46000e8b5cbce5
Kevin Lynch <klynch@squarespace.com>
2015-03-19 23:36:52 -0400

    add webapp params

--- src/webapp/webapp.py
+++ src/webapp/webapp.py
@@ -1,10 +1,10 @@
+import argparse
 import redis
 import logging
 import simplejson as json
 import sys
 from msgpack import Unpacker
 from flask import Flask, request, render_template
-from daemon import runner
 from os.path import dirname, abspath
 
 # add the shared settings file to namespace
@@ -54,39 +54,16 @@ def data():
         return resp, 500
 
 
-class App():
-    def __init__(self):
-        self.stdin_path = '/dev/null'
-        self.stdout_path = settings.LOG_PATH + '/webapp.log'
-        self.stderr_path = settings.LOG_PATH + '/webapp.log'
-        self.pidfile_path = settings.PID_PATH + '/webapp.pid'
-        self.pidfile_timeout = 5
-
-    def run(self):
-
-        logger.info('starting webapp')
-        logger.info('hosted at %s' % settings.WEBAPP_IP)
-        logger.info('running on port %d' % settings.WEBAPP_PORT)
-
-        app.run(settings.WEBAPP_IP, settings.WEBAPP_PORT)
-
 if __name__ == "__main__":
     """
     Start the server
     """
+    parser = argparse.ArgumentParser(description='Webapp to display anomalies.')
+    parser.add_argument("-H", "--host", default="0.0.0.0", help="The IP address for the webapp")
+    parser.add_argument("-p", "--port", type=int, default=1500, help="The port for the webapp")
+    args = parser.parse_args()
 
-    webapp = App()
-
+    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger("AppLog")
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s :: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-    handler = logging.FileHandler(settings.LOG_PATH + '/webapp.log')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    if len(sys.argv) > 1 and sys.argv[1] == 'run':
-        webapp.run()
-    else:
-        daemon_runner = runner.DaemonRunner(webapp)
-        daemon_runner.daemon_context.files_preserve = [handler.stream]
-        daemon_runner.do_action()
+    logger.info('starting webapp')
+    app.run(args.host, args.port)
