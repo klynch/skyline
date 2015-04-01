eef5855fb6ebfbae996102d06e75e03772214aba
Kevin Lynch <klynch@squarespace.com>
2015-04-01 10:22:12 -0400

    make tests executable

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import unittest2 as unittest
 from mock import Mock, patch
 from argparse import Namespace
--- tests/numpy_vs_msgpack.py
+++ tests/numpy_vs_msgpack.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import time
 import timeit
 import msgpack
